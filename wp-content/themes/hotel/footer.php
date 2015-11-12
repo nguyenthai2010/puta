4ebbd05343c75cc1a7130307ff9135970d3a1669
Thai Nguyen <thai@bliss-interactive.net>
2015-11-12 15:46:50 +0700

    Css for Footer

--- wp-content/themes/hotel/footer.php
+++ wp-content/themes/hotel/footer.php
@@ -2,61 +2,56 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <h3>About Us</h3>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            <div class="col-md-3 col-sm-6">
+                <h3><img src="img/logo-text.png"></h3>
+                <address>
+                    <span>
+                        Jl. Raya Bunutan, Amed,<br>
+                        80852 Karangasem, Bali
+                    </span>
+                    <span>
+                        +62 (0)81 338 405 335
+                    </span>
+                    <span>
+                        <a href="mailto:info@puriwirata.com">info@puriwirata.com</a>
+                    </span>
+                </address>
+                <div class="clearfix"></div>
             </div>
-            <div class="col-md-4">
-                <h3>Latest News</h3>
-                <ul class="bloglist-small">
-                    <li>
-                        <div class="date-box">
-                            <span class="day">28</span>
-                            <span class="month">SEP</span>
-                        </div>
-                        <div class="txt">
-                            <h5><a href="css/#">Lorem ipsum sit amet</a></h5>
-                            <div>
-                                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</div>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="date-box">
-                            <span class="day">26</span>
-                            <span class="month">SEP</span>
-                        </div>
-                        <div class="txt">
-                            <h5><a href="css/#">Lorem ipsum sit amet</a></h5>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        </div>
-                    </li>
+            <div class="col-md-2 col-sm-3">
+                <h3>ACCOMODATION</h3>
+
+                <ul >
+                    <li><a href="#">Deluxe</a></li>
+                    <li><a href="#">Villa Superior</a></li>
+                    <li><a href="#">VIlla Master</a></li>
+                    <li><a href="#">FUll Villa</a></li>
+
                 </ul>
                 <div class="clearfix"></div>
             </div>
-            <div class="col-md-4">
-                <h3>Our Address</h3>
-                <address>
-                    <span>20 Main Street, Melbourne</span>
-                    <span><strong>Phone:</strong>(200) 333 8890</span>
-                    <span><strong>Fax:</strong>(200) 333 8892</span>
-                    <span><strong>Email:</strong><a href="mailto:contact@example.com">contact@example.com</a></span>
-                    <span><strong>Web:</strong><a href="#test">http://example.com</a></span>
-                </address>
+            <div class="col-md-2 col-sm-3">
+                <h3>DIVING</h3>
+
+                <ul >
+                    <li><a href="#">Dive Sites</a></li>
+                    <li><a href="#">Fundives</a></li>
+                    <li><a href="#">Courses</a></li>
+                    <li><a href="#">PAckages</a></li>
+
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="col-md-3 pull-right-md col-sm-12" >
+                <h3>FOLLOW US</h3>
 
                 <div class="social-icons">
                     <a href="#">
-                        <img src="img/social-icons/facebook.png" alt=""></a>
+                        <img src="img/social-icons/sharing-ico1.png" alt=""></a>
                     <a href="#">
-                        <img src="img/social-icons/twitter.png" alt=""></a>
+                        <img src="img/social-icons/sharing-ico2.png" alt=""></a>
                     <a href="#">
-                        <img src="img/social-icons/dribbble.png" alt=""></a>
-                    <a href="#">
-                        <img src="img/social-icons/blogger.png" alt=""></a>
-                    <a href="#">
-                        <img src="img/social-icons/youtube.png" alt=""></a>
-                    <a href="#">
-                        <img src="img/social-icons/vimeo.png" alt=""></a>
+                        <img src="img/social-icons/sharing-ico3.png" alt=""></a>
                 </div>
             </div>
         </div>
@@ -65,20 +60,7 @@
     <div class="subfooter">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    &copy; Copyright 2015 - Corsiva by Liontheme
-                </div>
-                <div class="col-md-6">
-                    <nav>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Booking</a></li>
-                            <li><a href="#">News</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </nav>
-                </div>
+                Copyright © 2015. <span>Puri Wirata</span>. All Rights Reserved.
             </div>
         </div>
     </div>
