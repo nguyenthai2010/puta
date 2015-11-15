790dd6d8dee23ae34aaa991cf1483a8659235892
Binh Lam <binhdarkcu@gmail.com>
2015-11-15 13:54:21 +0700

    contact page

--- wp-content/themes/hotel/page-faq.php
+++ wp-content/themes/hotel/page-faq.php
@@ -17,7 +17,7 @@
                     <div class="col01">
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Where is Puri WIrata?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Where is Puri WIrata?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
                                     <p>Puri Wirata is located in Bunutan, Amed.  Amed is located in East Bali, about 100km from the airport/Denpasar (or about a 2,5h drive).</p>
                                 </div>
@@ -27,7 +27,7 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>How can I get to Puri Wirata?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>How can I get to Puri Wirata?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
                                     <p>
                                         There are different ways to get to Puri Wirata, depending on your own preferences. The easiest solution is if we send you a driver for a pick up. Prices for a private pick up depend on where you stay on the Island. A Pick up from the airport, in a regular 4 seater car during daytime, would be Rp. 550.000.  Contact us for any transport request.
@@ -45,7 +45,7 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Is Puri Wirata a 5 star resort?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Is Puri Wirata a 5 star resort?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
                                     <p>Puri Wirata is not a 5 star resort. We are a small family owned resort, nothing corporate about us. We are however a PADI 5 Star resort, which references to the services provided by our divecenter (Bali Reef Divers). </p>
                                     <p>However, even we have no star rating, we try to offer the best possible service and try to main a very good price/quality comparison. </p>
@@ -56,8 +56,8 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>I want a room with sea view,<br/>
-                            which room can you recommend me?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i><div class="q-text"><span>I want a room with sea view,</span>
+                            <span>which room can you recommend me?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
                                     <p>All of our 31 rooms have sea view. Yes. That is right. You can see the ocean from each and every one of our rooms. The earlier you book, the more chance of having an ocean front room. We have no priority system in place for rooms except first come, first serve. </p>
                                 </div>
@@ -68,7 +68,7 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Which methods of payment do you accept?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Which methods of payment do you accept?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
                                     <p>As of July 2015, a new government regulation tells us we can only accept payments in Indonesian Rupiah. </p>
                                     <p>At Puri Wirata, you can pay I cash or you can pay by using VISA or MASTER cards. However, we hold the right to charge a 3% fee for credit card payments.</p>
@@ -80,7 +80,7 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Are you close to the center of Amed?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Are you close to the center of Amed?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
                                     <p>Amed doesn’t really has a center. One would consider Jemeluk bay as the center of Amed, as this is where the boats to the Gili Islands leave/arrive, so that gets quite busy. Puri Wirata is 2km from Jemeluk. However, in our opinion, the better restaurants of Amed are within walking distance from Amed. Snorkeling spots are spread out over the length of the coast, so the best is to get a scooter and get around. </p>
                                 </div>
@@ -92,7 +92,7 @@
                     <div class="col01">
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>How can we get to the Gili Islands from Amed?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>How can we get to the Gili Islands from Amed?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
                                     <p>
                                         YES! You can! There are now daily fastboats going from Amed to the Gili Islands and vice versa. Trips usually leave in the morning around 9am from Amed. During busy times of the year, there are several trips a day. The companies pick up from the resort. If you want us to book your trip, contact us by mail and we will make sure your spot is reserved. The price is Rp. 300.000/pp for 1 way.
@@ -106,8 +106,8 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>I heard snorkeling around Amed is good.<br/>
-                                How can I snorkel?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>I heard snorkeling around Amed is good.</span>
+                                <span>How can I snorkel?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
 
                                 </div>
@@ -119,7 +119,7 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Can I go diving if I never dove before?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Can I go diving if I never dove before?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
 
                                 </div>
@@ -132,7 +132,7 @@
 
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Do I need to book up front for diving?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Do I need to book up front for diving?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
 
                                 </div>
@@ -140,13 +140,9 @@
                             <div class="small-border"></div>
                         </div>
 
-
-
-
-
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Do I need to book up front for your Spa?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Do I need to book up front for your Spa?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
 
                                 </div>
@@ -154,13 +150,9 @@
                             <div class="small-border"></div>
                         </div>
 
-
-
-
-
                         <div class="question-row">
                             <div class="question-head">
-                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <span>Do you serve vegetarian food?</span><div class="clear"></div></div>
+                                <div class="question-new"><i><img src="img/faq/plus.png"/></i> <div class="q-text"><span>Do you serve vegetarian food?</span></div><div class="clear"></div></div>
                                 <div class="answer-box">
 
                                 </div>
