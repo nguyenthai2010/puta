140886f82639edd409098ef47471ef00a026ac8b
Thai Nguyen <thai@bliss-interactive.net>
2015-11-15 20:42:10 +0700

    .

---
+++ wp-content/themes/hotel/page-location.php
@@ -0,0 +1,32 @@
+
+<?php get_header();?>
+
+<!-- subheader begin -->
+<div id="subheader" class="location">
+    <h1>LOCATION</h1>
+    <h3>WHERE IS PURI WIRATA</h3>
+</div>
+<!-- subheader close -->
+
+<!-- content begin -->
+<div id="content">
+    <div class="container" >
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title-text">
+                    <h2 style="color: #333333;">Puri Wirata Jl. Raya Bunutan, Amed, 80852<br>
+                        Karangasem, Bali, Indonesia
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- content close -->
+<div id="map-container">
+    <iframe  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.id/maps?f=q&amp;source=s_q&amp;hl=en&amp;q=16+Riverside+Rd,+Singapore&amp;sll=-2.547988,118.037109&amp;sspn=29.394918,50.756836&amp;ie=UTF8&amp;geocode=Fb8IFgAdu40vBg&amp;split=0&amp;hq=&amp;hnear=16+Riverside+Rd,+Singapore&amp;ll=1.444031,103.779771&amp;spn=0.014522,0.024784&amp;t=m&amp;z=14&amp;output=embed&amp;iwloc=false&amp;scrollwheel=false"></iframe>
+</div>
+
+<?php get_footer();?>
+
