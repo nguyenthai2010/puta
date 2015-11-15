223e9fe7e36eefe421fd7e46f22530b688b03335
Thai Nguyen <thai@bliss-interactive.net>
2015-11-15 18:23:34 +0700

    .

--- wp-content/themes/hotel/index.php
+++ wp-content/themes/hotel/index.php
@@ -9,12 +9,9 @@
                 <div class="text-wrap">
                     <div class="inner">
                         <div class="mid">
-                            <h3>The Best Value</h3>
-                            <h1>Under The Sun</h1>
-                            <div class="desc">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </div>
+                            <h3>ENJOY ONE OF THE</h3>
+                            <h1><span>MANY ACTIVITIES</span></h1>
+
                         </div>
                     </div>
 
@@ -26,18 +23,31 @@
                 <div class="text-wrap">
                     <div class="inner">
                         <div class="mid">
-                            <h3>Stay With Us</h3>
-                            <h1>Feel Like Home</h1>
-                            <div class="desc">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </div>
+                            <h3>THE BEST VALUE</h3>
+                            <h1>UNDER THE <span>SUN</span></h1>
+
                         </div>
                     </div>
 
                 </div>
                 <img src="img/slider-home/pic%20(2).jpg" alt="">
             </li>
+            <li>
+                <div class="text-wrap">
+                    <div class="inner">
+                        <div class="mid">
+                            <h3>STAY WITH US</h3>
+                            <h1>AND GO <span>DIVING</span></h1>
+
+                        </div>
+                    </div>
+
+                </div>
+                <img src="img/slider-home/pic%20(2).jpg" alt="">
+            </li>
+
+
+
         </ul>
     </div>
 </div>
@@ -94,34 +104,6 @@
             <img src="img/room/pic%20(3).jpg" alt="">
         </div>
 
-        <div class="room-item item">
-            <div class="overlay">
-                <a href="single-room.html">
-                    <h1>Deluxe Suite</h1>
-                </a>
-                <div class="desc">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
-                    <div class="price">$499<span>/night</span></div>
-                    <a href="single-room.html" class="btn-border">View Details</a>
-                </div>
-            </div>
-            <img src="img/room/pic%20(4).jpg" alt="">
-        </div>
-
-        <div class="room-item item">
-            <div class="overlay">
-                <a href="single-room.html">
-                    <h1>Luxury Suite</h1>
-                </a>
-                <div class="desc">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
-                    <div class="price">$599<span>/night</span></div>
-                    <a href="single-room.html" class="btn-border">View Details</a>
-                </div>
-            </div>
-            <img src="img/room/pic%20(5).jpg" alt="">
-        </div>
-
 
 
     </div>
@@ -133,9 +115,16 @@
 <!-- content close -->
 <!-- content begin -->
 <div id="content">
-    <div class="container">
-
-        <div class="row" id="home-things">
+    <div class="container" id="home-things">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title-text">
+                    <h2>THINGS TO DO AT PURI WIRATA</h2>
+                    <div class="small-border"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row" >
             <div class="col-md-3 col-sm-6">
                 <div class="col">
                     <img src="img/home/1.jpg">
