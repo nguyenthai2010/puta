e693276624cf68122ae5d55cebe97810e75c1d9f
Thai Nguyen <thai@bliss-interactive.net>
2015-11-17 15:18:09 +0700

    About, Spa pages

--- wp-content/themes/hotel/page-about.php
+++ wp-content/themes/hotel/page-about.php
@@ -10,137 +10,53 @@
 
 <!-- content begin -->
 <div id="content" class="no-top no-bottom">
-    <section id="explore-1" class="side-bg">
-        <div class="col-md-6 col-md-offset-6 pull-right image-container">
-            <div class="background-image" style='background: url("img/about/about-1.jpg");'></div>
-        </div>
+    <?php
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="inner-padding">
-                        <h2>THE RESORT</h2>
+    $args = array(
+        'post_type' 	 => 'about',
+        'posts_per_page' => -1 ,
+        'order'			 => 'desc'
+    );
+    $queryRows = get_posts($args);
+    ?>
 
-                        Enjoy the sunrise from your balcony in the morning.
-                        See the fishermen take off and bring back their catch.
-                        Watch the waves rolling in as you read a book in silence.
-                        Enjoy a sky full of stars on a bright Amed night.<br><br>
 
-                        We believe that high quality, comfortable accommodation should be available to anyone. We have built our rooms to international standards, furnished them with fine Balinese crafts and maintain them very well, to provide you comfort you'd expect from accommodation twice the price. On top of that, all of our rooms have sea view.<br><br>
+    <?php
+    $i = 0;
+    foreach ($queryRows as $row) {
+        $i++;
+        $url_image = wp_get_attachment_url(get_post_thumbnail_id($row->ID));
+        $name = $row->post_title;
+        $description = $row->post_content;
 
-                        All rooms and villas are spread out on a nice slope between the mountain and the sea. Spread out in a beautiful tropical garden, you can come at ease and enjoy Bali like it once was and still is in Amed nowadays. Quiet, relaxing, laid back and generous.
-                        <div class="small-border"></div>
+        $pullClass = 'pull-right';
+        $extraClass = 'col-md-5';
+        if($i%2 == 0) {
+            $pullClass = 'pull-left';
+            $extraClass = 'col-md-5 col-md-offset-7';
+        }
+        ?>
+        <section id="explore-1" class="side-bg">
+            <div class="col-md-6 col-md-offset-6 <?=$pullClass?> image-container">
+                <div class="background-image" style='background: url("<?=$url_image?>");'></div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="<?=$extraClass?>">
+                        <div class="inner-padding">
+                            <h2><?=$name?></h2>
+
+
+                            <?=$description?>
+                            <div class="small-border"></div>
+                        </div>
+
                     </div>
-
                 </div>
             </div>
-        </div>
-    </section>
-
-
-    <section id="explore-2" class="side-bg">
-        <div class="col-md-6 col-md-offset-6 pull-left image-container">
-            <div class="background-image" style='background: url("img/about/about-2.jpg");'></div>
-        </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-md-offset-7">
-                    <div class="inner-padding">
-                        <h2>PRIME LOCATION</h2>
-
-                        Amed could well be Bali's best kept secret. A peaceful stretch of coast, flanked by mountains on the one side and the ocean on the other, with beautiful, secluded bays, magnificent views and quiet villages, providing for a taste of paradise.<br><br>
-
-                        Only 2 hours away from the hustle and bustle from the South, you feel like you've entered another world. Referred to as the REAL Bali, life here is in balance with nature.
-                        See the fishermen leave at the break of dawn, to come back a couple of hours later with their catch of the day.<br><br>
-
-                        Walk or sit on the beach, without being constantly hassled.
-                        Meet local people and get inspired by their hospitality and friendliness.
-
-                        <div class="small-border"></div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section id="explore-3" class="side-bg">
-        <div class="col-md-6 col-md-offset-6 pull-right image-container">
-            <div class="background-image" style='background: url("img/about/about-3.jpg");'></div>
-        </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="inner-padding">
-                        <h2>DIVING</h2>
-
-
-                        Healthy coral reefs start at only meters from the shore. A huge variety of fish and sea creatures inhabit the reefs, providing for a constantly evolving underwater landscape. Also the world famous USS Liberty Wreck is less than half an hour away.<br><br>
-
-                        Puri Wirata Dive Resort & Spa offers you a comfortable and affordable home amidst all this beauty. A hotel with a family feel like you've rarely encountered before.<br>
-                        With our own PADI 5 Star Dive center located in our resort, Spa, Beach, Transport service and 2 beachfront swimming pools, we are perfectly equipped to give you the relaxing holiday you deserve.
-                        <div class="small-border"></div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <section id="explore-4" class="side-bg">
-        <div class="col-md-6 col-md-offset-6 pull-left image-container">
-            <div class="background-image" style='background: url("img/about/about-4.jpg");'></div>
-        </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-md-offset-7">
-                    <div class="inner-padding">
-                        <h2>THE SPA</h2>
-
-                        A relaxing stay at Puri Wirata Dive Resort can never be complete without a visit to our Spa.<br><br>
-
-                        Nurture your Body, Spirit and Mind. Loose yourself in a treatment, with the sound of the ocean just a few meters away from you. Forget everything around you for a while and come out reborn.<br><br>
-
-                        Open daily from 9am till 9pm, our trained staff will provide you carefully selected, traditional and natural therapies, using natural products only.
-
-                        Wayan, Komang and Kadek hope to welcome you soon.
-                        <div class="small-border"></div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <section id="explore-5" class="side-bg">
-        <div class="col-md-6 col-md-offset-6 pull-right image-container">
-            <div class="background-image" style='background: url("img/about/about-5.jpg");'></div>
-        </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="inner-padding">
-                        <h2>DINING</h2>
-
-                        Our sea side restaurant, managed in the kitchen by Chef Edi (Ex-Dancing Dragon Chef), serves a variety of Balinese, Indonesian and Western dishes at very reasonable prices. All our ingredients are fresh. Vegetables are being selected and delivered daily and are locally grown. Because of this, some fruits or vegetables may be seasonal.
-                        Almost daily, fresh fish gets delivered as well. We are dependent on the catch of the day of the local fishermen.<br><br>
-
-                        We're also serving a number of exotic cocktails, to enjoy while sunbathing at one of the pools, while relaxing at the restaurant or to enjoy with family/friends on a nice, breezy tropical night.<br><br>
-
-                        FREE Wifi is available in the restaurant as well.
-                        <div class="small-border"></div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    <?php }?>
 
 
 </div>
